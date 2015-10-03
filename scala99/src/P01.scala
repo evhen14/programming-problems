18d0e6a0fd8a758e68d7b5af65da06d667682be7
evhen14 <yevgen.polyak@gmail.com>
2015-10-04 12:42:02 +1300

    Making problem 1 a generic solution

--- scala99/src/P01.scala
+++ scala99/src/P01.scala
@@ -1,5 +1,5 @@
 package object P01 {
-  def last(list: List[Int]): Int = list match {
+  def last[A](list: List[A]): A = list match {
     case x::Nil => x
     case x::xs => last(xs)
     case _ => throw new NoSuchElementException()
