83e225ff605ed9926d6c91af36fa22126c9821f9
evhen14 <yevgen.polyak@gmail.com>
2015-10-04 13:32:34 +1300

    Simplifying problem 3 solution

--- scala99/src/P03.scala
+++ scala99/src/P03.scala
@@ -1,8 +1,7 @@
 package object P03 {
   def nth[A](n:Int, list: List[A]): A = list match {
     case x::_ if n == 0 => x
-    case x::Nil => throw new NoSuchElementException()
-    case x::xs => nth(n - 1, xs)
+    case _::xs => nth(n - 1, xs)
     case _ => throw new NoSuchElementException()
   }
 }
